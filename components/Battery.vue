e492f8503d005356f59059ef8474a53333c4337a
Saman Shakya <samdshakya@gmail.com>
2025-04-30 20:19:06 +0545

    refactor: ui upgrade

--- components/Battery.vue
+++ components/Battery.vue
@@ -2,12 +2,9 @@
     <span class="battery-wrap flex items-center">
         <span class="text-base mr-2">{{ battPercent }}</span>
         <i
-            class="battery relative border-2 border-white w-9 h-5 rounded-sm after:w-[3px] after:h-[10px] after:top-1/2 after:-right-[5px] after:-mt-[5px] after:content-[''] after:block after:absolute after:bg-white after:rounded-tr-md after:rounded-br-md"
-            ><span
-                class="h-4 border-2 border-black w-0 top-0 left-0 transition-all absolute bg-white"
-                :style="{ width: battPercent }"
-            ></span
-        ></i>
+            class="battery relative border-2 border-white w-9 h-5 rounded-sm after:w-[3px] after:h-[10px] after:top-1/2 after:-right-[5px] after:-mt-[5px] after:content-[''] after:block after:absolute after:bg-white after:rounded-tr-md after:rounded-br-md"><span
+                class="h-[17px] w-0 top-0 left-0 transition-all absolute bg-white"
+                :style="{ width: battPercent }"></span></i>
     </span>
 </template>
 <script>
@@ -34,8 +31,8 @@ export default {
         navigator.battery
             ? this.getBatteryLevels(navigator.battery)
             : navigator.getBattery
-            ? navigator.getBattery().then(this.getBatteryLevels)
-            : '';
+                ? navigator.getBattery().then(this.getBatteryLevels)
+                : '';
     }
 };
 </script>
--- components/Settings.vue
+++ components/Settings.vue
@@ -1,134 +1,113 @@
 <template>
     <span class="settings text-2xl leading-none ml-6 cursor-pointer relative">
-        <i
-            @click="settingsActive = !settingsActive"
-            class="inline-block align-top transition-all"
-        >
+        <i @click="settingsActive = !settingsActive"
+            class="inline-block align-top transition-all hover:rotate-45 duration-300">
             <svg class="icon icon-settings">
                 <use xlink:href="#icon-settings"></use>
             </svg>
         </i>
-        <Transition>
-            <span
-                class="s-dropdown absolute z-10 pt-4 cursor-default -right-3 top-full after:content-[''] after:absolute after:-mb-4 after:bottom-full after:right-[14px] after:w-0 after:h-0 after:border-8 after:border-transparent after:border-b-white"
-                v-if="settingsActive"
-            >
-                <span
-                    class="s-dd block rounded-md p-4 text-sm bg-black border border-white min-w-[260px]"
-                >
-                    <ul class="m-0 p-0 list-none -mb-3">
-                        <li class="mb-4">
-                            <label class="c-checkbox"
-                                >Guided Meditation
-                                <input
-                                    type="checkbox"
-                                    v-model="guidedMeditationCheck"
-                                    @change="toggleInstructionAudio"
-                                />
-                                <span class="checkmark"></span>
-                            </label>
-                            <ul v-if="guidedMeditationCheck" class="pl-9 pt-4">
-                                <li
-                                    v-for="(instruction, index) in presetsList
-                                        .guidedInstruction.language"
-                                    :key="index"
-                                    class="mb-4"
-                                >
-                                    <label class="c-checkbox">
-                                        {{
-                                            capitalizeFirstLetter(
-                                                instruction.language
-                                            )
-                                        }}
-                                        Audio
-                                        <input
-                                            type="radio"
-                                            :value="index"
-                                            v-model="
-                                                presetsList.guidedInstruction
-                                                    .languageActive
-                                            "
-                                            @change="
-                                                selectInstructionAudio(index)
-                                            "
-                                        />
-                                        <span class="checkmark"></span>
-                                    </label>
+        <Transition name="drawer">
+            <div class="fixed inset-0 z-50 bg-black/50 backdrop-blur-sm" v-if="settingsActive"
+                @click.self="settingsActive = false">
+                <div
+                    class="drawer fixed top-0 right-0 bottom-0 w-full max-w-xs sm:max-w-sm bg-gradient-to-b from-[#1c1c2e] to-[#0f172a] shadow-2xl overflow-y-auto pb-10 transform transition-all duration-300 ease-in-out z-50 border-l border-white/5">
+                    <div class="p-5 border-b border-white/10 flex items-center justify-between">
+                        <h2 class="text-xl font-medium tracking-wide">Settings</h2>
+                        <button @click="settingsActive = false"
+                            class="text-white/60 hover:text-white transition-colors p-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                    </div>
+                    <div class="p-5">
+                        <ul class="m-0 p-0 list-none space-y-8">
+                            <li>
+                                <label class="c-checkbox flex items-center gap-3 justify-between w-full">
+                                    <span class="text-lg font-medium tracking-wide">Guided Meditation</span>
                                     <div
-                                        class="relative pt-4 pr-1 pb-1 before:w-6 before:h-6 before:absolute before:border-t before:border-r before:rounded-tr-md before:right-4 before:-top-2 before:content-['']"
-                                        v-if="
-                                            customAudioActive &&
-                                            presetsList.guidedInstruction
-                                                .language.length ==
-                                                index + 1
-                                        "
-                                    >
-                                        <label
-                                            for="custom-audio"
-                                            class="border-2 border-white rounded-[30px] text-sm leading-none text-white bg-transparent outline-none block px-3 py-3 cursor-pointer text-center -mb-[7px] hover:bg-white hover:text-black"
-                                            >Select Audio</label
-                                        >
-                                        <input
-                                            id="custom-audio"
-                                            type="file"
-                                            accept="audio/*"
-                                            class="hidden"
-                                            @change="setCustomAudio"
-                                        />
+                                        class="relative inline-block w-12 h-6 transition duration-200 ease-in-out rounded-full cursor-pointer">
+                                        <input type="checkbox" class="absolute w-6 h-6 opacity-0 cursor-pointer"
+                                            v-model="guidedMeditationCheck" @change="toggleInstructionAudio" />
                                         <span
-                                            class="whitespace-no-wrap text-center overflow-hidden block text-xs pt-3 text-ellipsis max-w-[165px]"
-                                            v-if="
-                                                !!presetsList.guidedInstruction
-                                                    .customAudioFileName
-                                            "
-                                            >{{
-                                                presetsList.guidedInstruction
-                                                    .customAudioFileName
-                                            }}</span
-                                        >
+                                            class="absolute w-full h-full transition-all duration-300 ease-in-out bg-white/10 rounded-full shadow-inner"></span>
+                                        <span
+                                            class="absolute left-0 w-6 h-6 transition-all duration-300 transform bg-white rounded-full shadow-lg"
+                                            :class="{ 'translate-x-6 bg-[#43e97b]': guidedMeditationCheck }"></span>
                                     </div>
-                                </li>
-                            </ul>
-                        </li>
+                                </label>
+                                <div v-if="guidedMeditationCheck" class="mt-5">
+                                    <div v-for="(instruction, index) in presetsList.guidedInstruction.language"
+                                        :key="index" class="mb-5">
+                                        <label class="flex items-center gap-3 cursor-pointer text-base">
+                                            <input type="radio" :value="index"
+                                                v-model="presetsList.guidedInstruction.languageActive"
+                                                @change="selectInstructionAudio(index)" class="hidden" />
+                                            <span
+                                                class="w-5 h-5 rounded-full border-2 border-white/60 relative transition-all duration-200 flex items-center justify-center"
+                                                :class="{ 'border-[#43e97b]': presetsList.guidedInstruction.languageActive === index }">
+                                                <span
+                                                    class="absolute w-0 h-0 bg-[#43e97b] rounded-full transition-all duration-200"
+                                                    :class="{ 'w-3 h-3': presetsList.guidedInstruction.languageActive === index }"></span>
+                                            </span>
+                                            <span class="font-light tracking-wide">{{
+                                                capitalizeFirstLetter(instruction.language) }} Audio</span>
+                                        </label>
+                                        <div class="mt-4 ml-7"
+                                            v-if="customAudioActive && presetsList.guidedInstruction.language.length == index + 1">
+                                            <label for="custom-audio"
+                                                class="border border-white/40 rounded-lg text-sm leading-none bg-white/5 hover:bg-white/15 outline-none block px-5 py-3 cursor-pointer text-center transition-all duration-300 shadow-sm">Select
+                                                Audio</label>
+                                            <input id="custom-audio" type="file" accept="audio/*" class="hidden"
+                                                @change="setCustomAudio" />
+                                            <span
+                                                class="whitespace-nowrap text-left overflow-hidden block text-xs pt-3 text-ellipsis text-white/60"
+                                                v-if="!!presetsList.guidedInstruction.customAudioFileName">{{
+                                                    presetsList.guidedInstruction.customAudioFileName }}</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
 
-                        <li class="mb-4">
-                            <label class="c-checkbox"
-                                >Background Sounds
-                                <input
-                                    type="checkbox"
-                                    v-model="backgroundSoundCheck"
-                                    @change="toggleBackgroundSound"
-                                />
-                                <span class="checkmark"></span>
-                            </label>
-                            <ul v-if="backgroundSoundCheck" class="pl-9 pt-4">
-                                <li
-                                    v-for="(sound, index) in presetsList
-                                        .backgroundSound.sound"
-                                    :key="index"
-                                    class="mb-4"
-                                >
-                                    <label class="c-checkbox">
-                                        {{ sound.soundTitle }}
-                                        <input
-                                            type="radio"
-                                            :value="index"
-                                            v-model="
-                                                presetsList.backgroundSound
-                                                    .soundActive
-                                            "
-                                            @change="
-                                                selectBackgroundSound(index)
-                                            "
-                                        />
-                                        <span class="checkmark"></span>
-                                    </label>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </span>
-            </span>
+                            <li>
+                                <label class="c-checkbox flex items-center gap-3 justify-between w-full">
+                                    <span class="text-lg font-medium tracking-wide">Background Sounds</span>
+                                    <div
+                                        class="relative inline-block w-12 h-6 transition duration-200 ease-in-out rounded-full cursor-pointer">
+                                        <input type="checkbox" class="absolute w-6 h-6 opacity-0 cursor-pointer"
+                                            v-model="backgroundSoundCheck" @change="toggleBackgroundSound" />
+                                        <span
+                                            class="absolute w-full h-full transition-all duration-300 ease-in-out bg-white/10 rounded-full shadow-inner"></span>
+                                        <span
+                                            class="absolute left-0 w-6 h-6 transition-all duration-300 transform bg-white rounded-full shadow-lg"
+                                            :class="{ 'translate-x-6 bg-[#43e97b]': backgroundSoundCheck }"></span>
+                                    </div>
+                                </label>
+                                <div v-if="backgroundSoundCheck" class="mt-5">
+                                    <div v-for="(sound, index) in presetsList.backgroundSound.sound" :key="index"
+                                        class="mb-5">
+                                        <label class="flex items-center gap-3 cursor-pointer text-base">
+                                            <input type="radio" :value="index"
+                                                v-model="presetsList.backgroundSound.soundActive"
+                                                @change="selectBackgroundSound(index)" class="hidden" />
+                                            <span
+                                                class="w-5 h-5 rounded-full border-2 border-white/60 relative transition-all duration-200 flex items-center justify-center"
+                                                :class="{ 'border-[#43e97b]': presetsList.backgroundSound.soundActive === index }">
+                                                <span
+                                                    class="absolute w-0 h-0 bg-[#43e97b] rounded-full transition-all duration-200"
+                                                    :class="{ 'w-3 h-3': presetsList.backgroundSound.soundActive === index }"></span>
+                                            </span>
+                                            <span class="font-light tracking-wide">{{ sound.soundTitle }}</span>
+                                        </label>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </Transition>
     </span>
 </template>
@@ -141,7 +120,7 @@ export default {
             settingsActive: false,
             customAudioActive:
                 this.presetsList.guidedInstruction.languageActive ===
-                    this.presetsList.guidedInstruction.language.length - 1 ??
+                this.presetsList.guidedInstruction.language.length - 1 ??
                 true,
             guidedMeditationCheck:
                 this.presetsList.guidedInstruction.statusActive,
@@ -188,11 +167,6 @@ export default {
             bgSound.activePath = bgSound.sound[index].url;
             this.$forceUpdate();
         },
-        clickOutsideClose(e) {
-            if (!this.$el.contains(e.target)) {
-                this.settingsActive = false;
-            }
-        },
         setCustomAudio(e) {
             let files = e.target.files || e.dataTransfer.files;
             let guidedInstruction = this.presetsList.guidedInstruction;
@@ -211,13 +185,44 @@ export default {
                     type: 'error'
                 });
             }
+        },
+        clickOutsideClose(e) {
+            if (!this.$el.contains(e.target)) {
+                this.settingsActive = false;
+            }
         }
     },
     mounted() {
         document.addEventListener('click', this.clickOutsideClose);
     },
-    destroyed() {
+    beforeDestroy() {
         document.removeEventListener('click', this.clickOutsideClose);
     }
 };
 </script>
+
+<style scoped>
+.drawer-enter-active,
+.drawer-leave-active {
+    transition: opacity 0.3s ease, transform 0.3s ease;
+}
+
+.drawer-enter-from,
+.drawer-leave-to {
+    opacity: 0;
+}
+
+.drawer-enter-from .drawer,
+.drawer-leave-to .drawer {
+    transform: translateX(100%);
+}
+
+.drawer {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    letter-spacing: 0.015em;
+}
+
+.drawer h2 {
+    letter-spacing: 0.01em;
+}
+</style>
